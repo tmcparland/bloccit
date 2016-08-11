cc4d4e940398b98dc8accaec3989c98f89a9a5b2
Tara McParland <tara.mcparland@gmail.com>
2016-08-11 01:43:49 +0000

    Pre assignment-34

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
+
+
   resources :topics do
     resources :posts, except: [:index]
+    resources :sponsored_posts, only: [:show, :new, :edit]
   end
 
   get 'about' => 'welcome#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,17 @@ posts = Post.all
     )
 end
 
+50.times do
+    SponsoredPost.create!(
+        topic: topics.sample,
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        price: 99
+    )
+end
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
