8bba503f1ea27c25997889a2e73085f7b9cfd765
Tara McParland <tara.mcparland@gmail.com>
2016-08-14 16:53:22 +0000

    assignment-34-final

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :topics do
     resources :posts, except: [:index]
-    resources :sponsored_posts, only: [:show, :new, :edit]
+    resources :sponsored_posts, except: [:index]
   end
 
   get 'about' => 'welcome#about'
