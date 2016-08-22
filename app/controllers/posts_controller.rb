a2121b8077ec42c408160d19a5f39dbb79faa472
Brittany Martin <brittany.jill.martin@gmail.com>
2016-08-22 11:58:42 -0400

    Checkpoint 40 - fixed Topic and Post Controller tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  
   before_action :require_sign_in, except: :show
   before_action :authorize_user, except: [:show, :new, :create]
 
@@ -11,29 +10,30 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
-  
-  def create
-    @post = @topic.posts.build(post_params)
-    @post.user = current_user
-    
-    if @post.save
-      flash[:notice] = "Post was saved successfully."
-      redirect_to [@topic, @post]
-      
-    else
-      flash.now[:alert] = "There was and errir saving the post. Please try again."
-      render :new
-    end
-  end
 
   def edit
     @post = Post.find(params[:id])
   end
-  
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.build(post_params)
+
+    @post.user = current_user
+
+    if @post.save
+      flash[:notice] = "Post was saved successfully."
+      redirect_to [@topic, @post]
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
   def update
     @post = Post.find(params[:id])
     @post.assign_attributes(post_params)
- 
+
     if @post.save
       flash[:notice] = "Post was updated successfully."
       redirect_to [@post.topic, @post]
@@ -42,10 +42,9 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
- 
 
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
@@ -56,17 +55,18 @@ class PostsController < ApplicationController
     end
   end
 
-   private
- 
-   def post_params
-     params.require(:post).permit(:title, :body)
-   end
-   
-   def authorize_user
-     post = Post.find(params[:id])
-     unless current_user == post.user || current_user.admin?
-       flash[:alert] = "You must be an admin to do that."
-       redirect_to [post.topic, post]
-     end
-   end
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def authorize_user
+    post = Post.find(params[:id])
+
+    unless current_user == post.user || current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
+  end
 end
