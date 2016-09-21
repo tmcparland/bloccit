a83d4e749c07f877cb56ec0e849587c14e3936a1
Tara McParland <tara.mcparland@gmail.com>
2016-09-21 14:06:32 +0000

    checkpoint-47

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ActiveRecord::Base
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
    after_create :send_favorite_emails
+   
+   default_scope { order('updated_at DESC') }
  
    private
  
