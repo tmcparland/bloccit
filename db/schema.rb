e552fbe57a1b7cab4eeaaf185f3c039e2c01d44b
Tara McParland <tara.mcparland@gmail.com>
2016-09-26 18:20:12 +0000

    checkpoint-48

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911223714) do
+ActiveRecord::Schema.define(version: 20160926163017) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20160911223714) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
